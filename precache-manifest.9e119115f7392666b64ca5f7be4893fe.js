ea7a28c142b93cd6e8cb3d49ba613b054349155c
Kwanruadee Khumongkhol <kkwankmk@gmail.com>
2019-11-05 19:37:53 +0700

    Updates

---
+++ precache-manifest.9e119115f7392666b64ca5f7be4893fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e7d8d1267385d88c59e9d40820c934",
+    "url": "/kpc/index.html"
+  },
+  {
+    "revision": "cc3d9b2e75cb894c6f5f",
+    "url": "/kpc/static/css/2.7ececfe9.chunk.css"
+  },
+  {
+    "revision": "00f08f71eff4898e354a",
+    "url": "/kpc/static/css/main.45262538.chunk.css"
+  },
+  {
+    "revision": "cc3d9b2e75cb894c6f5f",
+    "url": "/kpc/static/js/2.3eefdc8e.chunk.js"
+  },
+  {
+    "revision": "00f08f71eff4898e354a",
+    "url": "/kpc/static/js/main.bd2a34b8.chunk.js"
+  },
+  {
+    "revision": "059c8031ed81e161d6e5",
+    "url": "/kpc/static/js/runtime-main.25b3f523.js"
+  }
+]);
